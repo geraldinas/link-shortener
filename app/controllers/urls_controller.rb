caa793893849d482cec5940a789a6d305cf8a16c
Geraldina <geraldina@Kickstarters-MacBook-Pro-2.local>
2015-06-05 13:42:35 -0400

    show action for short_id

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,21 +1,25 @@
-class UrlsController < ApplicationController
+class UrlsController < ApplicationController  
+
   def new
-  	@shortened_url = Url.new
+    @shortened_url = Url.new
+    if params[:shortened_id]
+      @shortened_id = params[:shortened_id]
+    end
   end
 
-  def create
+  def create 
     @shortened_url = Url.new(url: params[:url][:url])
-  	if @shortened_url.save
-  		flash[:shortened_id] = @shortened_url.id
-  		redirect_to new_url_url
+    if @shortened_url.save
+      @shortened_id = @shortened_url.id
+      #redirect_to new_url_path(shortened_id: @shortened_id)
+      render :show
     else
       render :action => "new"
-  	end
+    end
   end
 
   def show
     @shortened_url = Url.find(params[:id])
     redirect_to @shortened_url.url
   end
-
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,7 @@
 class Url < ActiveRecord::Base
 	validates :url, :presence => true	
+
+	def url_extension
+
+	end
 end
